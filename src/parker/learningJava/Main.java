b59cb2e2190c12c6eea56e4c8b22d8c2fa118637
Christian Parker <christianparker512@gmail.com>
2021-09-06 10:44:29 -0500

    Initial commit

---
+++ src/parker/learningJava/Main.java
@@ -0,0 +1,11 @@
+package parker.learningJava;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String numberAsString = "2021";
+        System.out.println("numberAsString = " + numberAsString);
+
+        int number = Integer.parseInt(numberAsString);
+    }
+}
